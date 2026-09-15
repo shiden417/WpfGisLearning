6189d418f41e9a45d65d0defe50e5a7618ce504e
shiden <shiden.4.17@gmail.com>
2026-09-15 15:37:13 +0900

    地図選択店舗を一覧へ自動スクロール

--- WpfGisLearning/Views/ShopListView.xaml.cs
+++ WpfGisLearning/Views/ShopListView.xaml.cs
@@ -8,23 +8,41 @@ namespace WpfGisLearning.Views;
 
 public partial class ShopListView : UserControl
 {
+    private readonly ShopListViewModel _viewModel;
+
     public ShopListView(ShopListViewModel viewModel)
     {
         InitializeComponent();
+        _viewModel = viewModel;
         DataContext = viewModel;
+        _viewModel.SelectedShopChanged += ViewModel_SelectedShopChanged;
+    }
+
+    private void ViewModel_SelectedShopChanged(object? sender, Shop? shop)
+    {
+        if (shop is null)
+        {
+            return;
+        }
+
+        Dispatcher.BeginInvoke(() => ShopListBox.ScrollIntoView(shop));
     }
 
     private void ShopListItem_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
     {
         if (DataContext is not ShopListViewModel viewModel)
+        {
             return;
+        }
 
         var item = ItemsControl.ContainerFromElement(
             (ItemsControl)sender,
             e.OriginalSource as DependencyObject) as ListBoxItem;
 
         if (item?.DataContext is not Shop shop)
+        {
             return;
+        }
 
         viewModel.SelectedShop = shop;
         viewModel.OpenSelectedShopCommand.Execute(null);
