fa2bcec45097fadd8e03f23c75e3ca8b22fe95ad
shiden <shiden.4.17@gmail.com>
2026-09-15 10:45:27 +0900

    お気に入り状態を通知可能に変更

--- WpfGisLearning/Models/Shop.cs
+++ WpfGisLearning/Models/Shop.cs
@@ -1,6 +1,8 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace WpfGisLearning.Models;
 
-public class Shop
+public partial class Shop : ObservableObject
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
@@ -8,11 +10,12 @@ public class Shop
     public string Address { get; set; } = string.Empty;
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-
     public string RamenType { get; set; } = "醤油";
     public string RecommendedMenu { get; set; } = string.Empty;
     public string OpeningHours { get; set; } = string.Empty;
     public string ClosedDay { get; set; } = string.Empty;
     public double Rating { get; set; }
-    public bool IsFavorite { get; set; }
+
+    [ObservableProperty]
+    private bool isFavorite;
 }
