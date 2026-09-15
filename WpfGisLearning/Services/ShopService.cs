eb5ed52450d6f6fdf70b7245a94e77bfd54721f7
shiden <shiden.4.17@gmail.com>
2026-09-15 10:31:37 +0900

    refactor: keep shop data in shared service

--- WpfGisLearning/Services/ShopService.cs
+++ WpfGisLearning/Services/ShopService.cs
@@ -1,20 +1,37 @@
+using WpfGisLearning.Models;
+
 namespace WpfGisLearning.Services;
 
 public class ShopService : IShopService
 {
-    // 簡易なサンプル：ショップ名やメッセージを返す
-    public string GetWelcomeMessage()
+    private readonly List<Shop> _shops =
+    [
+        new Shop { Id = 1, Name = "Tokyo Station Store", Price = 1200m, Address = "東京都千代田区", Latitude = 35.681236, Longitude = 139.767125 },
+        new Shop { Id = 2, Name = "Osaka Umeda Store", Price = 980m, Address = "大阪市北区", Latitude = 34.702485, Longitude = 135.495951 },
+        new Shop { Id = 3, Name = "Sapporo Store", Price = 1500m, Address = "札幌市中央区", Latitude = 43.062096, Longitude = 141.354376 }
+    ];
+
+    public string GetWelcomeMessage() => "Welcome to Ramenia!";
+
+    public IEnumerable<Shop> GetShops() => _shops;
+
+    public void AddShop(Shop shop)
     {
-        Console.WriteLine("test");
-        return "Welcome to Sample Shop! 今週のセール: 全品10%オフ";
+        _shops.Add(shop);
     }
-    public System.Collections.Generic.IEnumerable<WpfGisLearning.Models.Shop> GetShops()
+
+    public void UpdateShop(Shop shop)
     {
-        // サンプルデータ
-        return new[] {
-            new WpfGisLearning.Models.Shop { Id = 1, Name = "Tokyo Station Store", Price = 1200m, Address = "東京都千代田区", Latitude = 35.681236, Longitude = 139.767125 },
-            new WpfGisLearning.Models.Shop { Id = 2, Name = "Osaka Umeda Store", Price = 980m, Address = "大阪市北区", Latitude = 34.702485, Longitude = 135.495951 },
-            new WpfGisLearning.Models.Shop { Id = 3, Name = "Sapporo Store", Price = 1500m, Address = "札幌市中央区", Latitude = 43.062096, Longitude = 141.354376 }
-        };
+        var index = _shops.FindIndex(x => x.Id == shop.Id);
+
+        if (index >= 0)
+        {
+            _shops[index] = shop;
+        }
+    }
+
+    public void DeleteShop(int id)
+    {
+        _shops.RemoveAll(x => x.Id == id);
     }
 }
