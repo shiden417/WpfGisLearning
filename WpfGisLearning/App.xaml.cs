06c389d4dba7bd6e1e2be06b2f339fd3c4682368
shiden <shiden.4.17@gmail.com>
2026-09-15 12:06:10 +0900

    写真サービスとニュース画面をDI登録

--- WpfGisLearning/App.xaml.cs
+++ WpfGisLearning/App.xaml.cs
@@ -13,30 +13,25 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
-
         var services = new ServiceCollection();
-
         services.AddSingleton<MainViewModel>();
         services.AddTransient<LifetimeTestService>();
         services.AddSingleton<MainWindow>();
         services.AddSingleton<MessageViewModel>();
         services.AddSingleton<MessageView>();
-
         services.AddSingleton<INavigationService, NavigationService>();
-
         services.AddTransient<DetailView>();
         services.AddTransient<ShopListView>();
         services.AddTransient<ShopListViewModel>();
         services.AddTransient<ShopEditView>();
         services.AddTransient<ShopEditViewModel>();
-
         services.AddTransient<Views.ShopPage>();
         services.AddTransient<Views.DetailPage>();
-
+        services.AddTransient<NewsView>();
+        services.AddTransient<NewsViewModel>();
         services.AddSingleton<IShopService, ShopService>();
-
+        services.AddSingleton<IPhotoService, PhotoService>();
         _serviceProvider = services.BuildServiceProvider();
-
         var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
         mainWindow.Show();
     }
