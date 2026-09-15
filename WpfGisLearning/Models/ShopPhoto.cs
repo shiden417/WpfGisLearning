cbec3f5d865130dd6b8e2350c4c0b844452afd44
shiden <shiden.4.17@gmail.com>
2026-09-15 12:03:24 +0900

    店舗写真モデルを追加

---
+++ WpfGisLearning/Models/ShopPhoto.cs
@@ -0,0 +1,9 @@
+namespace WpfGisLearning.Models;
+
+public class ShopPhoto
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString("N");
+    public string FileName { get; set; } = string.Empty;
+    public bool IsMain { get; set; }
+    public int SortOrder { get; set; }
+}
