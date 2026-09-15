e199e5dd71f4b53dc9bd0f0a40da3b253c33f7c4
shiden <shiden.4.17@gmail.com>
2026-09-15 10:33:44 +0900

    店舗編集画面への遷移を追加

--- WpfGisLearning/Services/INavigationService.cs
+++ WpfGisLearning/Services/INavigationService.cs
@@ -3,6 +3,7 @@ namespace WpfGisLearning.Services;
 public interface INavigationService
 {
     void NavigateToDetail(int id);
+    void NavigateToShopEdit(int? id = null);
     void NavigateToShopList();
     void NavigateToShopPageFrame();
 }
