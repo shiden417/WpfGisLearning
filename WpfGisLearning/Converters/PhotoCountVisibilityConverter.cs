fe039d7610bb657fc9680d9e35d0d1ddc4559a54
shiden <shiden.4.17@gmail.com>
2026-09-15 16:23:17 +0900

    fix: prevent photo file locking during save

--- WpfGisLearning/Converters/PhotoCountVisibilityConverter.cs
+++ WpfGisLearning/Converters/PhotoCountVisibilityConverter.cs
@@ -1,6 +1,8 @@
 using System.Globalization;
+using System.IO;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace WpfGisLearning.Converters;
 
@@ -8,6 +10,29 @@ public class PhotoCountVisibilityConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         => value is int count && count == 0 ? Visibility.Visible : Visibility.Collapsed;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => Binding.DoNothing;
+}
+
+public sealed class ImagePathConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string path || string.IsNullOrWhiteSpace(path) || !File.Exists(path))
+        {
+            return null!;
+        }
+
+        using var stream = File.OpenRead(path);
+        var bitmap = new BitmapImage();
+        bitmap.BeginInit();
+        bitmap.CacheOption = BitmapCacheOption.OnLoad;
+        bitmap.StreamSource = stream;
+        bitmap.EndInit();
+        bitmap.Freeze();
+        return bitmap;
+    }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => Binding.DoNothing;
