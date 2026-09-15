cb16a7c0c080e7c6f1e6519cdd89601e6c66c24b
shiden <shiden.4.17@gmail.com>
2026-09-15 10:42:08 +0900

    店舗情報を拡張

--- WpfGisLearning/Models/Shop.cs
+++ WpfGisLearning/Models/Shop.cs
@@ -3,15 +3,16 @@ namespace WpfGisLearning.Models;
 public class Shop
 {
     public int Id { get; set; }
-
     public string Name { get; set; } = string.Empty;
-
     public decimal Price { get; set; }
-
-    // 追加プロパティ
     public string Address { get; set; } = string.Empty;
-
     public double Latitude { get; set; }
-
     public double Longitude { get; set; }
+
+    public string RamenType { get; set; } = "醤油";
+    public string RecommendedMenu { get; set; } = string.Empty;
+    public string OpeningHours { get; set; } = string.Empty;
+    public string ClosedDay { get; set; } = string.Empty;
+    public double Rating { get; set; }
+    public bool IsFavorite { get; set; }
 }
