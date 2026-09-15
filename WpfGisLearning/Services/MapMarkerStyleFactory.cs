47228a49e6b08d9519fa543f862466471973b357
shiden <shiden.4.17@gmail.com>
2026-09-15 17:24:36 +0900

    fix: remove detail map marker circle

--- WpfGisLearning/Services/MapMarkerStyleFactory.cs
+++ WpfGisLearning/Services/MapMarkerStyleFactory.cs
@@ -19,6 +19,18 @@ public static class MapMarkerStyleFactory
             selected ? SelectedShopScale : NormalShopScale);
     }
 
+    public static ImageStyle CreateDetailShopMarker()
+    {
+        const string pinSvg = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"36\" height=\"44\" viewBox=\"0 0 36 44\"><path d=\"M18 2C9.716 2 3 8.716 3 17c0 10 15 25 15 25s15-15 15-25C33 8.716 26.284 2 18 2z\" fill=\"#C74B4B\" stroke=\"#C74B4B\" stroke-width=\"1\"/></svg>";
+
+        return new ImageStyle
+        {
+            Image = new Image { Source = $"svg-content://{pinSvg}" },
+            RelativeOffset = new RelativeOffset(0, 0.5),
+            SymbolScale = NormalShopScale
+        };
+    }
+
     public static SymbolStyle CreateCurrentLocationMarker()
     {
         return new SymbolStyle
