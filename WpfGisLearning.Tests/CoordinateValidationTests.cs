776cbee1e25efafe1c931b7ac5363392ca471d22
shiden <shiden.4.17@gmail.com>
2026-09-15 14:09:53 +0900

    test: clean up coordinate validation tests

--- WpfGisLearning.Tests/CoordinateValidationTests.cs
+++ WpfGisLearning.Tests/CoordinateValidationTests.cs
@@ -1,4 +1,3 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WpfGisLearning.Services;
 
 namespace WpfGisLearning.Tests;
@@ -10,7 +9,7 @@ public class CoordinateValidationTests
     [DataRow(35.681236, 139.767125)]
     [DataRow(-90.0, -180.0)]
     [DataRow(90.0, 180.0)]
-    public void IsValidCoordinate_ValidCoordinates_ReturnsTrue(double latitude, double longitude)
+    public void IsValid_ValidCoordinates_ReturnsTrue(double latitude, double longitude)
     {
         Assert.IsTrue(MapCoordinateValidator.IsValid(latitude, longitude));
     }
@@ -21,20 +20,20 @@ public class CoordinateValidationTests
     [DataRow(35.0, -180.1)]
     [DataRow(35.0, 180.1)]
     [DataRow(0.0, 0.0)]
-    public void IsValidCoordinate_InvalidCoordinates_ReturnsFalse(double latitude, double longitude)
+    public void IsValid_InvalidCoordinates_ReturnsFalse(double latitude, double longitude)
     {
         Assert.IsFalse(MapCoordinateValidator.IsValid(latitude, longitude));
     }
 
     [TestMethod]
-    public void IsValidCoordinate_NaN_ReturnsFalse()
+    public void IsValid_NaN_ReturnsFalse()
     {
         Assert.IsFalse(MapCoordinateValidator.IsValid(double.NaN, 139.0));
         Assert.IsFalse(MapCoordinateValidator.IsValid(35.0, double.NaN));
     }
 
     [TestMethod]
-    public void IsValidCoordinate_Infinity_ReturnsFalse()
+    public void IsValid_Infinity_ReturnsFalse()
     {
         Assert.IsFalse(MapCoordinateValidator.IsValid(double.PositiveInfinity, 139.0));
         Assert.IsFalse(MapCoordinateValidator.IsValid(35.0, double.NegativeInfinity));
