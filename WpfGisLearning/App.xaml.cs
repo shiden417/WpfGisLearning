4faf38ebdb8126a7fa94a44cb5668cdd5067b0bf
shiden <shiden.4.17@gmail.com>
2026-09-15 10:34:08 +0900

    店舗登録・編集画面をDIに追加

--- WpfGisLearning/App.xaml.cs
+++ WpfGisLearning/App.xaml.cs
@@ -16,32 +16,28 @@ public partial class App : Application
 
         var services = new ServiceCollection();
 
-        // ViewModels / Views / Services
         services.AddSingleton<MainViewModel>();
         services.AddTransient<LifetimeTestService>();
         services.AddSingleton<MainWindow>();
         services.AddSingleton<MessageViewModel>();
         services.AddSingleton<MessageView>();
 
-        // Navigation
-        services.AddSingleton<Services.INavigationService, Services.NavigationService>();
+        services.AddSingleton<INavigationService, NavigationService>();
 
-        // Detail view and viewmodel are created at navigation time. Register the view so
-        // ActivatorUtilities can resolve any DI dependencies it needs.
-        services.AddTransient<Views.DetailView>();
-        services.AddTransient<Views.ShopListView>();
-        services.AddTransient<ViewModels.ShopListViewModel>();
-        // Frame+Page sample
+        services.AddTransient<DetailView>();
+        services.AddTransient<ShopListView>();
+        services.AddTransient<ShopListViewModel>();
+        services.AddTransient<ShopEditView>();
+        services.AddTransient<ShopEditViewModel>();
+
         services.AddTransient<Views.ShopPage>();
         services.AddTransient<Views.DetailPage>();
 
-        // Shop service
-        services.AddSingleton<Services.IShopService, Services.ShopService>();
+        services.AddSingleton<IShopService, ShopService>();
 
         _serviceProvider = services.BuildServiceProvider();
 
         var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
-
         mainWindow.Show();
     }
 }
