6979834f5a78bdd83e72de197b8bc25a707285cb
shiden <shiden.4.17@gmail.com>
2026-09-16 09:41:31 +0900

    refactor: move service interfaces

--- WpfGisLearning/Services/INavigationService.cs
+++
@@ -1,9 +0,0 @@
-namespace WpfGisLearning.Services;
-
-public interface INavigationService
-{
-    void NavigateToDetail(int id);
-    void NavigateToShopEdit(int? id = null);
-    void NavigateToShopList();
-    void NavigateToShopPageFrame();
-}
