aea2d6a9dd8648f068fe182a7a890068e7cef78f
shiden <shiden.4.17@gmail.com>
2026-09-16 15:07:20 +0900

    refactor: share shop location map layer creation

---
+++ WpfGisLearning/Map/ShopLocationLayerBuilder.cs
@@ -0,0 +1,34 @@
+using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Layers;
+using Mapsui.Projections;
+using WpfGisLearning. Models;
+
+namespace WpfGisLearning.Map;
+
+public sealed record ShopLocationLayerResult(MemoryLayer Layer, MPoint Point);
+
+public static class ShopLocationLayerBuilder
+{
+    public const string DefaultLayerName = "Shop";
+
+    public static ShopLocationLayerResult Build(
+        double latitude,
+        double longitude,
+        string layerName = DefaultLayerName,
+        bool selected = false)
+    {
+        var point = SphericalMercator.FromLonLat(longitude, latitude).ToMPoint();
+        var feature = new PointFeature(point);
+        feature.Styles.Add(MapMarkerStyleFactory.CreateShopMarker(selected));
+
+        var layer = new MemoryLayer
+        {
+            Name = layerName,
+            Style = null,
+            Features = new[] { feature }
+        };
+
+        return new ShopLocationLayerResult(layer, point);
+    }
+}
