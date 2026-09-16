e4817b02689b6b07e96c662c6f36f9ffaef1eac8
shiden <shiden.4.17@gmail.com>
2026-09-16 15:36:36 +0900

    docs: add learning comments to location model

--- WpfGisLearning/Models/CurrentLocation.cs
+++ WpfGisLearning/Models/CurrentLocation.cs
@@ -1,3 +1,9 @@
 namespace WpfGisLearning.Models;
 
+/// <summary>
+/// 現在地として取得した緯度・経度をまとめて扱う不変データです。
+/// record を使うことで、位置情報のような単純な値オブジェクトを表現しています。
+/// </summary>
+/// <param name="Latitude">緯度です。</param>
+/// <param name="Longitude">経度です。</param>
 public sealed record CurrentLocation(double Latitude, double Longitude);
