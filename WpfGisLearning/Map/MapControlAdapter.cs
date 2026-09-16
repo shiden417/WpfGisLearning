12ee06c8413d06b550e1dd9ecf729e457f5f741f
shiden <shiden.4.17@gmail.com>
2026-09-16 14:30:53 +0900

    refactor: add WPF map control adapter

---
+++ WpfGisLearning/Map/MapControlAdapter.cs
@@ -0,0 +1,29 @@
+using Mapsui;
+using Mapsui.Layers;
+using Mapsui.UI.Wpf;
+
+namespace WpfGisLearning.Map;
+
+public sealed class MapControlAdapter : IMapControlAdapter
+{
+    private readonly MapControl _mapControl;
+
+    public MapControlAdapter(MapControl mapControl)
+    {
+        _mapControl = mapControl;
+    }
+
+    public Mapsui.Map? Map
+    {
+        get => _mapControl.Map;
+        set => _mapControl.Map = value;
+    }
+
+    public double ActualWidth => _mapControl.ActualWidth;
+    public double ActualHeight => _mapControl.ActualHeight;
+
+    public void Refresh() => _mapControl.Refresh();
+
+    public MapInfo? GetMapInfo(Mapsui.Manipulations.ScreenPosition position, IEnumerable<ILayer> layers) =>
+        _mapControl.GetMapInfo(position, layers);
+}
