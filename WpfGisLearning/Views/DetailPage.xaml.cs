4b8d3ad771c1d6a23e9238626f40813db6a79529
shiden <shiden.4.17@gmail.com>
2026-09-16 15:58:00 +0900

    docs: add learning comments to DetailPage

--- WpfGisLearning/Views/DetailPage.xaml.cs
+++ WpfGisLearning/Views/DetailPage.xaml.cs
@@ -3,24 +3,28 @@ using System.Windows.Controls;
 
 namespace WpfGisLearning.Views;
 
+/// <summary>
+/// Frame内のページ遷移を学習するための店舗詳細Pageです。
+/// </summary>
 public partial class DetailPage : Page
 {
+    /// <summary>表示対象店舗のIDです。</summary>
     public int Id { get; }
 
+    /// <summary>店舗IDを受け取り、画面へ表示します。</summary>
     public DetailPage(int id)
     {
         InitializeComponent();
-
         Id = id;
-
         IdText.Text = Id.ToString();
     }
 
+    /// <summary>戻る履歴が存在する場合、WPF FrameのNavigationServiceで前のPageへ戻ります。</summary>
     private void GoBack_Click(object sender, RoutedEventArgs e)
     {
-        if (this.NavigationService?.CanGoBack == true)
+        if (NavigationService?.CanGoBack == true)
         {
-            this.NavigationService.GoBack();
+            NavigationService.GoBack();
         }
     }
 }
