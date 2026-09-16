c950644f2b2d77692c28f8a744337b6fec433a7f
shiden <shiden.4.17@gmail.com>
2026-09-16 09:51:00 +0900

    修正：ViewModelのDI登録に必要な名前空間を追加

--- WpfGisLearning/App.xaml.cs
+++ WpfGisLearning/App.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 using WpfGisLearning.Services;
 using WpfGisLearning.Services.Interfaces;
+using WpfGisLearning.ViewModels;
 using WpfGisLearning.Views;
 
 namespace WpfGisLearning;
