de835d10c6862455d88653ba3605564d16de3da3
shiden <shiden.4.17@gmail.com>
2026-09-16 10:40:03 +0900

    テスト：24時間営業と未設定営業時間の判定を補強

--- WpfGisLearning.Tests/BusinessHoursStatusCalculatorTests.cs
+++ WpfGisLearning.Tests/BusinessHoursStatusCalculatorTests.cs
@@ -9,7 +9,6 @@ public class BusinessHoursStatusCalculatorTests
     public void IsOpen_ReturnsTrue_InsideBusinessHours()
     {
         var result = BusinessHoursStatusCalculator.IsOpen("11:00-21:00", "月曜", new DateTime(2026, 9, 16, 12, 30, 0));
-
         Assert.IsTrue(result);
     }
 
@@ -17,7 +16,6 @@ public class BusinessHoursStatusCalculatorTests
     public void IsOpen_ReturnsFalse_OutsideBusinessHours()
     {
         var result = BusinessHoursStatusCalculator.IsOpen("11:00-21:00", "月曜", new DateTime(2026, 9, 16, 22, 0, 0));
-
         Assert.IsFalse(result);
     }
 
@@ -25,7 +23,6 @@ public class BusinessHoursStatusCalculatorTests
     public void IsOpen_ReturnsFalse_OnClosedDay()
     {
         var result = BusinessHoursStatusCalculator.IsOpen("11:00-21:00", "水曜", new DateTime(2026, 9, 16, 12, 30, 0));
-
         Assert.IsFalse(result);
     }
 
@@ -33,7 +30,6 @@ public class BusinessHoursStatusCalculatorTests
     public void IsOpen_SupportsOvernightHours()
     {
         var result = BusinessHoursStatusCalculator.IsOpen("22:00-02:00", "", new DateTime(2026, 9, 16, 23, 30, 0));
-
         Assert.IsTrue(result);
     }
 
@@ -41,7 +37,26 @@ public class BusinessHoursStatusCalculatorTests
     public void IsOpen_ReturnsFalse_WhenHoursAreNotSpecified()
     {
         var result = BusinessHoursStatusCalculator.IsOpen(string.Empty, string.Empty, new DateTime(2026, 9, 16, 12, 30, 0));
+        Assert.IsFalse(result);
+    }
 
+    [TestMethod]
+    public void IsOpen_ReturnsTrue_For24HourBusiness()
+    {
+        var result = BusinessHoursStatusCalculator.IsOpen(
+            BusinessHoursStatusCalculator.Open24Hours,
+            string.Empty,
+            new DateTime(2026, 9, 16, 3, 15, 0));
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsOpen_ReturnsFalse_OnClosedDay_EvenFor24HourBusiness()
+    {
+        var result = BusinessHoursStatusCalculator.IsOpen(
+            BusinessHoursStatusCalculator.Open24Hours,
+            "水",
+            new DateTime(2026, 9, 16, 12, 0, 0));
         Assert.IsFalse(result);
     }
 }
