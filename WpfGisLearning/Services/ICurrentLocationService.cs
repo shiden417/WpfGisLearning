7f999e594552bb82f28f2c4fe512ba0a17f5536a
shiden <shiden.4.17@gmail.com>
2026-09-16 09:41:35 +0900

    refactor: move service interfaces

--- WpfGisLearning/Services/ICurrentLocationService.cs
+++
@@ -1,8 +0,0 @@
-namespace WpfGisLearning.Services;
-
-public interface ICurrentLocationService
-{
-    Task<CurrentLocation?> GetCurrentLocationAsync();
-}
-
-public sealed record CurrentLocation(double Latitude, double Longitude);
