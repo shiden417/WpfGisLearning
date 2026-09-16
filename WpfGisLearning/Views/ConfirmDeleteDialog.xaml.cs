faf5a696a5600e0ced71041cf342c1a45d4b759a
shiden <shiden.4.17@gmail.com>
2026-09-16 15:58:10 +0900

    docs: add learning comments to ConfirmDeleteDialog

--- WpfGisLearning/Views/ConfirmDeleteDialog.xaml.cs
+++ WpfGisLearning/Views/ConfirmDeleteDialog.xaml.cs
@@ -2,21 +2,27 @@ using System.Windows;
 
 namespace WpfGisLearning.Views
 {
+    /// <summary>
+    /// 店舗削除の確認を行うためのモーダルダイアログです。
+    /// </summary>
     public partial class ConfirmDeleteDialog : Window
     {
+        /// <summary>ダイアログを生成してXAMLで定義されたUIを初期化します。</summary>
         public ConfirmDeleteDialog()
         {
             InitializeComponent();
         }
 
+        /// <summary>削除を確定し、呼び出し元へtrueを返してダイアログを閉じます。</summary>
         private void Yes_Click(object sender, RoutedEventArgs e)
         {
-            this.DialogResult = true;
+            DialogResult = true;
         }
 
+        /// <summary>削除をキャンセルし、呼び出し元へfalseを返してダイアログを閉じます。</summary>
         private void No_Click(object sender, RoutedEventArgs e)
         {
-            this.DialogResult = false;
+            DialogResult = false;
         }
     }
 }
