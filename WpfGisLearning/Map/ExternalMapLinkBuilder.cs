da5a62a4952393b955dbcd3db3631e30320ee432
shiden <shiden.4.17@gmail.com>
2026-09-16 15:36:46 +0900

    docs: add learning comments to map link builder

--- WpfGisLearning/Map/ExternalMapLinkBuilder.cs
+++ WpfGisLearning/Map/ExternalMapLinkBuilder.cs
@@ -2,8 +2,17 @@ using WpfGisLearning.Models;
 
 namespace WpfGisLearning.Map;
 
+/// <summary>
+/// 店舗情報から外部地図サービスのURLを組み立てるヘルパーです。
+/// UIからURLの文字列生成処理を切り離しています。
+/// </summary>
 public static class ExternalMapLinkBuilder
 {
+    /// <summary>
+    /// 店舗の緯度・経度を使ったGoogle Maps検索URLを生成します。
+    /// </summary>
+    /// <param name="shop">URLに使用する店舗情報です。</param>
+    /// <returns>Google Mapsで店舗位置を開くためのURLです。</returns>
     public static string CreateGoogleMapsUrl(Shop shop)
     {
         ArgumentNullException.ThrowIfNull(shop);
