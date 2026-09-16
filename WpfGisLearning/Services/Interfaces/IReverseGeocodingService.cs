9b821e0b97a690e04d33095549be82399f62d7da
shiden <shiden.4.17@gmail.com>
2026-09-16 15:40:39 +0900

    docs: add learning comments to reverse geocoding interface

--- WpfGisLearning/Services/Interfaces/IReverseGeocodingService.cs
+++ WpfGisLearning/Services/Interfaces/IReverseGeocodingService.cs
@@ -1,6 +1,13 @@
 namespace WpfGisLearning.Services.Interfaces;
 
+/// <summary>
+/// 緯度・経度から人が読める住所へ変換する逆ジオコーディングサービスの契約です。
+/// </summary>
 public interface IReverseGeocodingService
 {
+    /// <summary>指定した座標の住所を非同期で取得します。</summary>
+    /// <param name="latitude">対象地点の緯度です。</param>
+    /// <param name="longitude">対象地点の経度です。</param>
+    /// <returns>取得した住所。取得できない場合はnullです。</returns>
     Task<string?> GetAddressAsync(double latitude, double longitude);
 }
