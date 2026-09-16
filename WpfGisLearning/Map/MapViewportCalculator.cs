470c4b1d84dda19bbcb2d2cb10bf336fca6bfc30
shiden <shiden.4.17@gmail.com>
2026-09-16 10:01:58 +0900

    リファクタリング：地図処理をMapフォルダへ整理

---
+++ WpfGisLearning/Map/MapViewportCalculator.cs
@@ -0,0 +1,45 @@
+using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Projections;
+
+namespace WpfGisLearning.Map;
+
+public static class MapViewportCalculator
+{
+    public static MPoint CalculateCenter(ShopMapBounds bounds)
+    {
+        return SphericalMercator
+            .FromLonLat(
+                (bounds.MinLongitude + bounds.MaxLongitude) / 2,
+                (bounds.MinLatitude + bounds.MaxLatitude) / 2)
+            .ToMPoint();
+    }
+
+    public static double CalculateResolution(
+        ShopMapBounds bounds,
+        IReadOnlyList<double> availableResolutions,
+        double mapWidth,
+        double mapHeight,
+        double paddingFactor,
+        int singleShopResolutionIndex)
+    {
+        if (bounds.MinLongitude == bounds.MaxLongitude
+            && bounds.MinLatitude == bounds.MaxLatitude)
+        {
+            var index = Math.Min(singleShopResolutionIndex, availableResolutions.Count - 1);
+            return availableResolutions[index];
+        }
+
+        var minMap = SphericalMercator
+            .FromLonLat(bounds.MinLongitude, bounds.MinLatitude)
+            .ToMPoint();
+        var maxMap = SphericalMercator
+            .FromLonLat(bounds.MaxLongitude, bounds.MaxLatitude)
+            .ToMPoint();
+
+        var widthResolution = Math.Abs(maxMap.X - minMap.X) / mapWidth;
+        var heightResolution = Math.Abs(maxMap.Y - minMap.Y) / mapHeight;
+
+        return Math.Max(widthResolution, heightResolution) * paddingFactor;
+    }
+}
