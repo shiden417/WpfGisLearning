ae8b1629a97f736a4f54a195723e50f21e6c2772
shiden <shiden.4.17@gmail.com>
2026-09-16 11:55:27 +0900

    revert: remove cluster marker style

--- WpfGisLearning/Map/MapMarkerStyleFactory.cs
+++ WpfGisLearning/Map/MapMarkerStyleFactory.cs
@@ -6,11 +6,9 @@ public static class MapMarkerStyleFactory
 {
     private const string NormalShopColor = "#C74B4B";
     private const string SelectedShopColor = "#C56B4D";
-    private const string ClusterColor = "#343A40";
     private const string CurrentLocationColor = "#4A90E2";
     private const double NormalShopScale = 1.15;
     private const double SelectedShopScale = 1.4;
-    private const double ClusterScale = 1.6;
     private const double CurrentLocationScale = 0.65;
 
     public static ImageStyle CreateShopMarker(bool selected)
@@ -19,21 +17,6 @@ public static class MapMarkerStyleFactory
             Color.FromString(selected ? SelectedShopColor : NormalShopColor),
             Color.White,
             selected ? SelectedShopScale : NormalShopScale);
-    }
-
-    public static SymbolStyle CreateClusterMarker()
-    {
-        return new SymbolStyle
-        {
-            SymbolType = SymbolType.Ellipse,
-            SymbolScale = ClusterScale,
-            Fill = new Brush(Color.FromString(ClusterColor)),
-            Outline = new Pen
-            {
-                Color = Color.White,
-                Width = 2
-            }
-        };
     }
 
     public static SymbolStyle CreateCurrentLocationMarker()
